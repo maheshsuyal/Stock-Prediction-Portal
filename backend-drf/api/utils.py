418242ffb4d3a5551d3fd5a4afc706f13987b9d7
Mahesh Suyal <maheshsuyal13@gmail.com>
2026-06-30 16:35:58 +0530

    Remove virtual environment from repository

--- backend-drf/api/utils.py
+++ backend-drf/api/utils.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 
 
 def save_plot(plot_img_path):
+    # Create media folder if it doesn't exist
+    os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
+
     image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+
     plt.savefig(image_path)
     plt.close()
-    image_url = settings.MEDIA_URL + plot_img_path
-    return image_url
+
+    return settings.MEDIA_URL + plot_img_path
