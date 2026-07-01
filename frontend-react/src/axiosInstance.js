afa2c6dc710e61ab44d3fc3be928cb75bd7ec23d
Mahesh Suyal <maheshsuyal13@gmail.com>
2026-07-01 15:32:17 +0530

    Connect frontend to Render backend

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = "http://127.0.0.1:8000/api/v1"
+const baseURL = `${import.meta.env.VITE_BACKEND_ROOT}/api/v1`;
 const axiosInstance = axios.create({
     baseURL: baseURL,
     headers: {
